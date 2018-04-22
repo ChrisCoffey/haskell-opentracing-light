a4fef4a13abda8128068c09938544c4ec0bd29b8
Chris Coffey <cmcof273@gmail.com>
2018-04-21 21:14:55 -0400

    First pass at DataDog client

---
+++ src/Tracing/DataDog.hs
@@ -0,0 +1,72 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Tracing.DataDog (
+    publishDataDog,
+    DataDogSpan(..)
+    ) where
+
+import Tracing.Core (Span(..), SpanId(..), OpName(..), TraceId(..), SpanContext(..),
+    SpanRelation(..), SpanTag(..))
+
+import Control.Monad.Trans (liftIO, MonadIO)
+import Control.Monad (void)
+import Data.Monoid ((<>), mempty)
+import Data.Aeson
+import Data.Maybe (fromMaybe)
+import Data.Int (Int64)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lex.Integral as BS
+import qualified Data.HashMap.Strict as HM
+import Network.HTTP.Client
+
+
+-- | Publish 'Span' in the <https://zipkin.io/pages/data_model.html DataDog format> . No call is made
+-- on an empty
+publishDataDog :: MonadIO m =>
+    String -- ^ The address of the backend server
+    -> Manager
+    -> [Span] -- ^ The traced spans to send to a DataDog backend
+    -> m (Maybe (Response T.Text))
+publishDataDog _ _ [] = pure Nothing
+publishDataDog destination manager spans =
+    liftIO . fmap (Just . fmap decode) $ httpLbs zipkinReq manager
+    where
+        decode = T.decodeUtf8 . LBS.toStrict
+        req = parseRequest_ destination
+        body = RequestBodyLBS . encode $ DataDogSpan <$> spans
+        zipkinReq = req { method = "POST", requestBody = body, requestHeaders = [("content-type", "application/json")]}
+
+newtype DataDogSpan = DataDogSpan Span
+instance ToJSON DataDogSpan where
+    toJSON (DataDogSpan span) = object $ [
+        "trace_id" .= (unTrace . traceId $ context span),
+        "span_id" .= (unSpan . spanId $ context span),
+        "name" .=  unOp (operationName span),
+        "resource" .= unOp (operationName span),
+        "start" .= (floor . toMicros $ timestamp span :: Int64),
+        "type" .= ("CLIENT"::T.Text),
+        "duration" .= (toMicros $ duration span),
+        "service" .= (serviceName span),
+        "meta" .= (unTag <$> tags span)
+        ] <>
+        parentId (relations span)
+        where
+            toMicros = (*) 1000000
+            unOp (OpName n) = n
+            unSpan (SpanId sid) = sid
+            unTrace (TraceId tid) = tid
+            parentId :: [SpanRelation] -> [(T.Text, Value)]
+            parentId (ChildOf ctx:_) = ["parentId" .= (unSpan $ spanId ctx)]
+            parentId (FollowsFrom ctx:_) = ["parentId" .= (unSpan $ spanId ctx)]
+            parentId _ = []
+            padLeft 0 txt = txt
+            padLeft n txt
+                | T.length txt < n = padLeft n ("0"<>txt)
+                | otherwise = txt
+            unTag (TagString a) = toJSON a
+            unTag (TagBool a) = toJSON a
+            unTag (TagInt a) = toJSON a
+            unTag (TagDouble a) = toJSON a
